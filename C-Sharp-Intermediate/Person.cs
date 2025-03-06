f4b8f0614b1aa2982d4a7c286ef6891dd16e4658
Sean O'Connor <sean.f.oconnor4.civ@army.mil>
2025-03-06 14:17:48 -0600

    Code for first few lessons

---
+++ C-Sharp-Intermediate/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Intermediate
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+}
---
+++ C-Sharp-Intermediate/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace C_Sharp_Intermediate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //2.7
+            var person = new Person();
+            person.Name = "Sean";
+            var p = Person.Parse("John");
+            person.Introduce(p.Name);
+            Console.WriteLine("TEMP");
+
+            //2.8
+            var customer = new Customer(1, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+            //2.9
+            var person1 = new Person
+            {
+                Name="Test",
+                Age=21
+            };
+        }
+    }
+}
+
